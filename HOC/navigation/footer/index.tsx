5d9276dd310bd1cf2cb2d0ceb91c88c4d37ef15c
ashrafun nesa <ashrafunnesa.soron@gmail.com>
2024-03-10 20:01:17 +0600

    in prohress

--- HOC/navigation/footer/index.tsx
+++ HOC/navigation/footer/index.tsx
@@ -1,16 +1,31 @@
 "use client";
 
+import Image from "next/image";
+import FooterLogo from "../../../assets/images/footer-image.png"
+import { TwitterIcon } from "@/assets/icons";
 
 type Props = { data: any; setting: any };
 
 const Footer = ({ data, setting }: Props) => {
   return (
     <footer className="bg-dark-primary text-light-primary">
-      <section className="container_section_md custom-footer">
+      <section className="container_section_md custom-footer pt-11">
         <aside className="footer-aside">
           <div className="logo mb-5">
-
+            <Image src={FooterLogo} alt="footer-logo" />
+            <p className="mt-3 md:w-4/5">Aliquam vehicula luctus sodales lobortis justo mi sociis suscipit elementum, nisl euismod venenatis taciti.</p>
           </div>
+          <div>
+            <header className="font-bold mb-5">OVERVIEW</header>
+            <div className="flex mt-5 gap-4 ">
+                <TwitterIcon />
+                <TwitterIcon />
+                <TwitterIcon />
+                <TwitterIcon />
+                
+            </div>
+          </div>
+          
         </aside>
         <nav className="footer-nav">
           {" "}
--- assets/icons/index.tsx
+++ assets/icons/index.tsx
@@ -1,2 +1,3 @@
 export { default as DownIcon } from "./down";
 export { default as SearchIcon } from "./search";
+export { default as TwitterIcon } from "./Twitter";
