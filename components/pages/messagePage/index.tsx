c00aa8b4e9ea3a6397166402accb7b1c0a864079
ashrafun nesa <ashrafunnesa.soron@gmail.com>
2024-05-08 11:47:03 +0600

    Fix issue

--- components/pages/messagePage/index.tsx
+++ components/pages/messagePage/index.tsx
@@ -4,9 +4,17 @@ const Message = () => {
     <div className="container_section">
       <h1 className="lg:text-[44px] text-[30px] font-semibold lg:text-left text-center mb-6">Notifications & Messages</h1>
       <div className="max-w-[874px] mx-auto">
-        <div className="tab-list flex flex-wrap lg:justify-start justify-center  max-w-[790px] mx-auto">
+        <div className="tab-list flex flex-wrap lg:justify-start justify-center  max-w-[790px] mx-auto mb-5">
           <span className="tab-item message-tab active">Notifications (4)</span>
           <span className="tab-item message-tab">Inbox (4)</span>
+        </div>
+        <div className="box-shadow bg-white md:px-5 pt-4 pb-6 mb-5 rounded-[5px] flex md:flex-row flex-col gap-4">
+          <div></div>
+          <div>
+            <div>
+              <p className="text-[18px] font-semibold">Pro Gamer</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- components/pages/newsPage/featuredSection/index.tsx
+++ components/pages/newsPage/featuredSection/index.tsx
@@ -7,17 +7,15 @@ type Props = {};
 const FeaturedSection = (props: Props) => {
   return (
     <div className="feature-section flex flex-col lg:gap-7 gap-5 mb-8">
-      <div className="flex lg:flex-row flex-col lg:gap-7 gap-5 w-full">
-        <div className="lg:w-1/2 w-full">
+      <div className="flex flex-wrap lg:flex-row flex-col lg:gap-7 gap-5 w-full">
+        <div className="news-feature">
           <CustomLink href={`/reviews/single-review`}>
             <FeatureImage bigHeading={false} type="review" />
           </CustomLink>
         </div>
-        <div className=" lg:w-1/2 w-full"><FeatureImage bigHeading={false} type="blogs" /></div>
-      </div>
-      <div className="flex lg:flex-row flex-col lg:gap-7 gap-5 w-full">
-        <div className=" lg:w-1/2 w-full"><FeatureImage bigHeading={false} type="review" /></div>
-        <div className=" lg:w-1/2 w-full"><FeatureImage bigHeading={false} type="blogs" /></div>
+        <div className=" news-feature"><FeatureImage bigHeading={false} type="blogs" /></div>
+        <div className=" news-feature"><FeatureImage bigHeading={false} type="review" /></div>
+        <div className=" news-feature"><FeatureImage bigHeading={false} type="blogs" /></div>
       </div>
     </div>
   );
