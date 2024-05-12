511d85c8e022984d9f456e0fbbe65bf06ee4b482
ashrafun nesa <ashrafunnesa.soron@gmail.com>
2024-05-12 13:04:18 +0600

    Fix helper page

---
+++ app/helper/page.tsx
@@ -0,0 +1,19 @@
+const LoginPage = async () => {
+  return (
+    <>
+      <h1 className="heading-1">className=&quot;heading-1&quot; ----- h1</h1>
+      <br />
+      <h2 className="heading-2">className=&quot;heading-2&quot; ----- h2</h2>
+      <br />
+      <h3 className="heading-3">className=&quot;heading-3&quot; ----- h3</h3>
+      <br />
+      <h4 className="heading-4">className=&quot;heading-4&quot; ----- h4</h4>
+      <br />
+      <h5 className="heading-5">className=&quot;heading-5&quot; ----- h5</h5>
+      <br />
+      <p className="paragraph">className=&quot;paragraph&quot; ----- p </p>
+    </>
+  );
+};
+
+export default LoginPage;
